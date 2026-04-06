accb48cd7e330ba43c962a1ef2eee2fd0057c696
fajar-ab <fajarfanz378@gmail.com>
2026-04-06 21:25:44 +0700

    fitur buka extensi prosedural

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello, world")
+import pyautogui
+import time
+import os
+
+def open_extension():
+    os.system("xdotool windowactivate --sync $(xdotool search --onlyvisible --class 'Brave' | tail -1)")
+    time.sleep(0.5)
+    pyautogui.hotkey('alt', 'shift', 's')
+
+open_extension()
